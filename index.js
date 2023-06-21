e8073c7f485318873d38e74ac002f8cc937725e0
loner2424 <92987477+loner2424@users.noreply.github.com>
2023-06-22 04:50:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ const PORT = 3001;
 connectToMongoDB(process.env.MONGO_URI).then(() =>
   console.log("Mongodb connected")
 );
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(express.json());
 
 app.use("/url", urlRoute);
